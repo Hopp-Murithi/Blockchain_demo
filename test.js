24c88b495f02c142ddd1007a0f9068784d9c20bc
Hopp Murithi <hoppmurithi@gmail.com>
2023-02-21 16:27:29 +0300

    test: add test for functionality

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const Blockchain= require('./blockchain');
 let coreDao = new Blockchain();
 
 coreDao.createNewTransaction(
-    "10",
+    "120",
     "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
     "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
   );
