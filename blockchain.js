f023bbe5a7959643c450ffd5b8ef85bd9625bb2a
Hopp Murithi <hoppmurithi@gmail.com>
2023-02-21 14:26:18 +0300

    feat: add function to create first block

---
+++ blockchain.js
@@ -0,0 +1,17 @@
+class Blockchain {
+    constructor() {
+        this.chain = [this.generateGenesisBlock()]
+        this.pendingTransactions = []
+    }
+    generateGenesisBlock() {
+        return {
+          index: 1,
+          timestamp: Date.now(),
+          transactions: [],
+          nonce: 0,
+          hash: "hash",
+          previousBlockHash: "previousBlockHash",
+        };
+      }
+    
+}
